fb449dfa1eb1041fb76df3e0651055d2fdbb60ba
Chris309283 <80517220+Chris309283@users.noreply.github.com>
2021-05-17 15:34:28 +0200

    started javadocs

--- src/Match.java
+++ src/Match.java
@@ -1,3 +1,26 @@
+import java.util.ArrayList;
+
 public class Match
 {
+  private Time start, end;
+  private Date date;
+  private ArrayList<Player> bench, lineUp;
+  private String opponent, matchType;
+  private int scoreHomeTeam, scoreOpponent, teamSize;
+
+  public Match(Time start, Time end, Date date, String opponent,
+      String matchType)
+  {
+    this.start = start;
+    this.end = end;
+    this.date = date;
+    this.opponent = opponent;
+    this.matchType = matchType;
+    this.bench = new ArrayList<>();
+    this.lineUp = new ArrayList<>();
+    this.scoreHomeTeam = 0;
+    this.scoreOpponent = 0;
+
+
+  }
 }
