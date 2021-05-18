035522faa8924b648f18664f2f45b86f7a46e73c
StefanyRomeroV <80517166+StefanyRomeroV@users.noreply.github.com>
2021-05-18 14:08:13 +0200

    is not finish :P

--- src/PlayerList.java
+++ src/PlayerList.java
@@ -4,10 +4,32 @@ public class PlayerList
 {
   private ArrayList<Player> players;
 
-
   public PlayerList()
   {
     players = new ArrayList<Player>();
   }
 
+  public void addPlayer(Player player)
+  {
+    players.add(player);
+  }
+
+  public void removePlayer(Player player)
+      {
+        players.remove(player);
+      }
+
+
+  public ArrayList<Player> getPlayersAvailable()
+  {
+    ArrayList<Player> PlayerAvailable = new  ArrayList<Player>();
+    {
+      for (int i = 0; i < players.size(); i++)
+      {
+        if ()
+      }
+
+
+    }
+  }
 }
