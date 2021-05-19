13a43e1c211a87277db030cfc62719ae420aafbc
vlad151 <lazar.vlad151@gmail.com>
2021-05-19 12:35:35 +0300

    player commit

--- src/Player.java
+++ src/Player.java
@@ -7,13 +7,12 @@ public class Player
   private ArrayList<String> positions;
   private ArrayList<Unavailability> unavailabilities;
 
-  public Player(String name, int number, String position)
+  public Player(String name, int number)
   {
     this.name = name;
     this.number = number;
     positions = new ArrayList<String>();
-    positions.add(position);
-    unavailabilities=new ArrayList<Unavailability>();
+    unavailabilities = new ArrayList<Unavailability>();
   }
 
   public String getName()
@@ -43,47 +42,89 @@ public class Player
       positions.remove(position);
     }
   }
-  public boolean isSuspended(){
-    if(unavailabilities.get((unavailabilities.size()-1)).getType().equals("Suspended")){
+
+  public boolean isSuspended()
+  {
+    //add today date
+    if (unavailabilities.get((unavailabilities.size() - 1)).getType()
+        .equals("Suspended"))
+    {
+
       return true;
     }
-    else{
+    else
+    {
       return false;
     }
   }
 
-  public void addUnavailability(Unavailability unavailability){
+  public void addUnavailability(Unavailability unavailability)
+  {
     unavailabilities.add(unavailability);
   }
-  public void removeUnavailability(Unavailability unavailability){
+
+  public void removeUnavailability(Unavailability unavailability)
+  {
+    //add contains
     unavailabilities.remove(unavailability);
   }
-  public ArrayList<Unavailability> getAllUnavailabilities(){
+
+  public ArrayList<Unavailability> getAllUnavailabilities()
+  {
     return unavailabilities;
   }
 
-  public ArrayList<String > getPositions(){
+  public ArrayList<String> getPositions()
+  {
     return positions;
   }
 
-  public String getPosition(int index){
+  public String getPosition(int index)
+  {
     return positions.get(index);
   }
-  public Player copy(){
-    return new Player(name,number,positions.get(0));
-  }
-  public boolean equals(Object obj){
-    if(!(obj instanceof Player)){
-      return false;
+
+  public Player copy()
+  {
+    Player newPlayer = new Player(name, number);
+    for (int i = 0; i < positions.size(); i++)
+    {
+      newPlayer.addPosition(positions.get(i));
     }
-    else{
-      Player other=(Player)obj;
-      return name.equals(other.name)&&number==other.number&& positions.equals(other.positions);
+    for (int i = 0; i < unavailabilities.size(); i++)
+    {
+      newPlayer.addUnavailability(unavailabilities.get(i));
     }
-  }
-  public String toString(){
-    return "name: "+name+" number: "+number+" positions: "+positions;
+    return newPlayer;
   }
 
+  public boolean equals(Object obj)
+  {
+    if (!(obj instanceof Player))
+    {
+      return false;
+    }
+    else
+    {
+      Player other = (Player) obj;
+      return name.equals(other.name) && number == other.number && positions
+          .equals(other.positions) && unavailabilities
+          .equals(other.unavailabilities);
+    }
+  }
+
+  public String toString()
+  {
+    return "name: " + name + " number: " + number + " positions: " + positions;
+  }
+  public int getBenchedInARow(){
+    return benchedInARow;
+  }
+  public void setBenchedInARow(int games){
+    benchedInARow=games;
+  }
+  public void incrementBenchedInARow(){
+    benchedInARow++;
+  }
 
 }
