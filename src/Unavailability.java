5da9528f4e4f2f093f3e609e4bf93447eed3db10
vlad151 <lazar.vlad151@gmail.com>
2021-05-20 22:52:45 +0300

    update

--- src/Unavailability.java
+++ src/Unavailability.java
@@ -1,6 +1,6 @@
-
 /**
  * a class for creating unavailability objects
+ *
  * @author Java Gods
  * @version 1.0
  */
@@ -13,102 +13,124 @@ public class Unavailability
 
   /**
    * Three-argument constructor initializing the Unavailability
-   * @param start sets the start date of the Unavailability
+   *
+   * @param start         sets the start date of the Unavailability
    * @param numberOfGames sets the number of games the Unavailability is for
-   * isAvailable method is set initially to false
+   *                      isAvailable method is set initially to false
    */
-  public Unavailability(Date start, int numberOfGames){
-    this.type="Suspended";
-    this.start=start.copy();
-    this.numberOfGames=numberOfGames;
-    isAvailable=false;
+  public Unavailability(Date start, int numberOfGames)
+  {
+    this.type = "Suspended";
+    this.start = start.copy();
+    this.numberOfGames = numberOfGames;
+    isAvailable = false;
   }
 
   /**
-   *Two-argument constructor initializing the Unavailability
-   * @param type sets the type of Unavailability either Suspended or Injured
+   * Two-argument constructor initializing the Unavailability
+   *
+   * @param type  sets the type of Unavailability either Suspended or Injured
    * @param start sets the start date of the Unavailability
-   * isAvailable method is set initially to false
+   *              isAvailable method is set initially to false
    */
-  public Unavailability(String type, Date start){
-    this.type=type;
-    this.start=start.copy();
-    isAvailable=false;
-    numberOfGames=-1;
+  public Unavailability(String type, Date start)
+  {
+    this.type = type;
+    this.start = start.copy();
+    isAvailable = false;
+    numberOfGames = -1;
 
   }
 
   /**
    * Gets the type of Unavailability
+   *
    * @return the type of Unavailability
    */
-  public String getType(){
+  public String getType()
+  {
     return type;
-}
-  /**
-   * ends the Unavailability and sets the end to the current date
-   * @param end sets the end of the Unavailability as the current date
-   */
-  public void setAvailable(Date end){
-
-    this.end=end;
-    isAvailable=true;
   }
 
   /**
-   *Gets the start date of the Unavailability
+   * ends the Unavailability and sets the end to the current date
+   *
+   * @param end sets the end of the Unavailability as the current date
+   */
+  public void setAvailable(Date end)
+  {
+
+    this.end = end;
+    isAvailable = true;
+  }
+
+  /**
+   * Gets the start date of the Unavailability
+   *
    * @return the start date of the Unavailability
    */
 
-  public Date getStart(){
+  public Date getStart()
+  {
     return start.copy();
   }
 
   /**
    * Sets the start date of the Unavailability
+   *
    * @param start sets the start date of the Unavailability
    */
-  public void setStart(Date start){
-    this.start=start.copy();
+  public void setStart(Date start)
+  {
+    this.start = start.copy();
   }
 
   /**
    * Gets the end date of the Unavailability
+   *
    * @return the end date of the Unavailability
    */
-  public Date getEnd(){
+  public Date getEnd()
+  {
     return end.copy();
   }
 
   /**
-   *Sets the end date of the Unavailability
+   * Sets the end date of the Unavailability
+   *
    * @param end sets the end date of the Unavailability
    */
-  public void setEnd(Date end){
-    this.end=end.copy();
+  public void setEnd(Date end)
+  {
+    this.end = end.copy();
   }
 
   /**
    * Outputs the number of days the Unavailability lasted
+   *
    * @return the number of days from start date to end date
    */
-  public int lasted(){
-   return start.daysUntil(end);
+  public int lasted()
+  {
+    return start.daysUntil(end);
   }
 
   /**
    * checks if the unavailability end date is not before today's date
+   *
    * @return true if the unavailability end is not before doday's date. False otherwise
    */
-  public boolean isActive(){
-    if(Date.today().isBefore(end)){
+  public boolean isActive()
+  {
+    if (Date.today().isBefore(end))
+    {
       return true;
     }
-    else {
+    else
+    {
       return false;
     }
-}
-
+  }
 
 }
 
