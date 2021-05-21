beefeeb4b0be023e420bc84e9f728328782e6679
Hamoudi Resho <hamoderaso@outlook.com>
2021-05-21 14:26:55 +0200

    .

---
+++ src/PlayerList.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+
+public class PlayerList
+{
+  private ArrayList<Player> players;
+
+  public PlayerList()
+  {
+    players = new ArrayList<Player>();
+  }
+
+  public void addPlayer(Player player)
+  {
+    players.add(player);
+  }
+
+  public void removePlayer(Player player)
+  {
+    players.remove(player);
+  }
+
+  public ArrayList<Player> getPlayersAvailable()
+  {
+    ArrayList<Player> PlayersAvailable = new ArrayList<Player>();
+    for ( int i = 0; i < players.size(); i++)
+    {
+      if (players.get(i).)
+
+    }
+    return players;
+  }
+  public ArrayList<Player> getPlayersUnAvailable()
+  {
+    ArrayList<Player>  PlayersUnAvailable new ArrayList<Player>();
+    for ( int i = 0; i < players.size(); i++)
+    {
+      if (players.get(i).isSuspended()|| players.get(i).is )
+
+    }
