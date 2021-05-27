da85d09ea25ea6c03d88cd56cce57b76b637ccff
Hamoudi Resho <hamoderaso@outlook.com>
2021-05-27 10:26:27 +0200

    some docts are added to MatchList

--- ViaClubFun/src/model/MatchList.java
+++ ViaClubFun/src/model/MatchList.java
@@ -3,25 +3,49 @@ package model;
 import java.io.Serializable;
 import java.util.ArrayList;
 
+ /**
+ * a class of which is a list of matches
+ * @author Java Gods
+ * @version 1.0
+ */
 public class MatchList implements Serializable
 {
+
+
   private ArrayList<Match> matches;
 
+
+  /**
+   * a constructor, which initialize the matches. it is without parameter
+   */
   public MatchList()
   {
     matches = new ArrayList<Match>();
   }
 
+  /**
+   * the method  add match to our list
+   * @param match of type Match which will be added to the list
+   */
   public void add(Match match)
   {
     matches.add(match);
   }
+
+  /**
+   * the method remove a match from the list
+   * @param match is input to the parameter which the user can remove a match from matches
+   */
 
   public void remove(Match match)
   {
     matches.remove(match);
   }
 
+  /**
+   * the method which gets the size of matches
+   * @return it return the size of matches
+   */
   public int size()
   {
     return matches.size();
